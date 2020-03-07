3b983913b6f1434be9fa4392f198820b8c4cdf91
me <pacurar.sebastian90@gmail.com>
2020-03-07 16:11:12 +0200

    MathGame_addedMinorStyling:   - added small css   - refactored .css methods for paragraphs to add font-weight

--- MathGame_jQuery/script.js
+++ MathGame_jQuery/script.js
@@ -27,17 +27,19 @@ $(document).ready(function () {
         if (parseInt($firstNum.text()) + parseInt($secondNum.text()) === parseInt($answerNum.val())) {
 
             // if answer is correct, display "answer is correct" in green. fades out in 1 second
-            $answerEval.text(`Your answer is correct`).css("color", "green");
+            $answerEval.text(`Your answer is correct`).css({"color": "green", "font-size": "20px"});
             $answerEval.fadeOut(1000);
 
         } else if (isNaN(parseInt($firstNum.text()) + parseInt($secondNum.text()))) {
 
             // if sum of values is NaN, it means the game did not start
-            $answerEval.text(`Please start a new game`).css("color", "blue");
+            $answerEval.text(`Please start a new game`).css({"color": "blue", "font-size": "20px"});
         } else {
 
             // if answer is wrong then display your result and the actual result in red. fades out in 3 seconds
-            $answerEval.text(`Your answer is ${$answerNum.val()} and the result is ${parseInt($firstNum.text()) + parseInt($secondNum.text())}`).css("color", "red");
+            $answerEval.text(`Your answer is ${$answerNum.val()} and the result is ` +
+                `${parseInt($firstNum.text()) + parseInt($secondNum.text())}`)
+                .css({"color": "red", "font-size": "30px"});
             $answerEval.fadeOut(3000);
         }
     })
