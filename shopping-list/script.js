5f4d26083538d65d8a64df499cf0032eacff1c40
Sebastian Pacurar <sebastianpacurar@users.noreply.github.com>
2020-07-21 21:35:51 +0300

    implementation for table: (#20)
    implementation for table and  improvements:
      - updated html document for the newest additions (table)
        - all list items are composed of 4 spans: product name, product price, remove span and buy span
        - added code for the html table
      - added price implementation for every product
        - implemented a second input in form which is of type number and is responsible for the addition of custom prices
        - implemented formatPrice function with a random price generator in case no price is given
      - switched list item layout to flexbox:
        - on the right side there is the price in dollars, remove button and buy button
      - implemented scroll for list
      - implemented functionality for buy product:
        - upon click the product name and product price will be copied to table
      - implemented table for bought products:
        - every item has a delete <span> element which should delete the clicked item from the table
      - added a boughtItems object which contains all the items which suffered the Buy event from the items list
      - updated CSS for all new and old elements

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,42 +1,91 @@
 'use strict';
 
 const list = document.querySelector('#products-list'),
-    form = document.forms['add-item'];
+    form = document.forms['add-item'],
+    table = document.querySelector('table'),
+    boughtItems = {};
 
 
+const formatPrice = (price) => {
+    if (price.length > 0) {
+        return `$${parseFloat(price).toFixed(2)}`;
+    } else {
+        return `$${(Math.random() * 20).toFixed(2)}`;
+    }
+}
+
+/*
+   in case the event target className is remove-btn-list, remove entire list item.
+   if className is buy-btn, then add a new row in table with product name and its price,
+     if item already exists in table, simply add the price to the existing price
+*/
 list.addEventListener('click', e => {
 
-    if (e.target.className === 'remove-btn') {
+    if (e.target.className === 'remove-btn-list') {
         const listItem = e.target.parentNode;
         list.removeChild(listItem);
     }
+
+    if (e.target.className === 'buy-btn') {
+        table.style.visibility = 'visible';
+
+        // set insertRow index to 1, otherwise it places the row above the table header
+        const row = table.insertRow(1),
+            nameCell = row.insertCell(0),
+            priceCell = row.insertCell(1);
+
+
+        // the first cell will contain the product name and a span meant to remove the row from table
+        nameCell.innerHTML =
+            `${e.target.parentNode.querySelector('.product-name').textContent} 
+                <span class="remove-btn-table">x</span>`;
+
+        priceCell.textContent = e.target.parentNode.querySelector('.product-price').textContent;
+
+
+        // add the item to boughtItems object. if it exists, add only the price to existent price
+        boughtItems[nameCell.textContent] = priceCell.textContent;
+    }
 });
 
 
+// simply put, an entire list item is created along with all its inner elements
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const input = form.querySelector('#addInput'),
-        product = input.value.trim();
+    const productInput = form.querySelector('#addInput').value.trim(),
+        priceInput = form.querySelector('#addPrice').value.trim();
 
-    if (product.length > 1) {
+    if (productInput.length > 1) {
 
         const listItem = document.createElement('LI');
-        listItem.textContent = product;
 
         const removeBtn = document.createElement('SPAN');
         removeBtn.textContent = 'Remove';
-        removeBtn.classList.add('remove-btn');
+        removeBtn.classList.add('remove-btn-list');
 
         const addBtn = document.createElement('SPAN');
-        addBtn.textContent = 'Add';
-        addBtn.classList.add('add-btn');
+        addBtn.textContent = 'Buy';
+        addBtn.classList.add('buy-btn');
 
-        listItem.appendChild(addBtn);
+        const price = document.createElement('SPAN');
+        price.classList.add('product-price');
+        price.textContent = formatPrice(priceInput);
+
+        const productName = document.createElement('SPAN');
+        productName.classList.add('product-name');
+        productName.textContent = productInput;
+
+        listItem.appendChild(productName);
+        listItem.appendChild(price);
         listItem.appendChild(removeBtn);
+        listItem.appendChild(addBtn);
 
         list.appendChild(listItem);
 
-        input.value = '';
+        form.querySelector('#addInput').value = '';
+        form.querySelector('#addPrice').value = '';
     }
 });
+
+
