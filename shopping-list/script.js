7f609b01bf755a7f2bb062d387afc99d1d766080
me <pacurar.sebastian90@gmail.com>
2020-07-23 15:39:19 +0300

    shopping-list - fix price generator and update readme   - fixed price generator to start from 1.00 and end to 20.00 instead of 0.00 and 19.00   - update README.md

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -49,7 +49,7 @@ const formatPrice = (price) => {
     if (price.length > 0) {
         return `$${parseFloat(price).toFixed(2)}`;
     } else {
-        return `$${(Math.random() * 20).toFixed(2)}`;
+        return `$${((Math.random() * 20) + 1).toFixed(2)}`;
     }
 };
 
