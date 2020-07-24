ea549a86a60a677f8e525c60792c63f00b439516
Sebastian Pacurar <sebastianpacurar@users.noreply.github.com>
2020-07-25 02:41:39 +0300

    playing-cards-generator - final Implementations (#28)
    playing-cards-generator - final Implementations:
      - switched to fetch API using async and await
      - implemented Reshuffle
      - fixed issues with negative numbers and number 0
      - added buttons disabling according to how many cards are left and if there is any deck existent on the table
      - added notification message:
        - states how many decks and cards are left
        - in case of an error with the fetch call, it will state the error
      - fixed issues with css
      - implemented responsive design for screens between 280px and 395px
      - updated README.md files

--- playing-cards-generator/README.md
+++ playing-cards-generator/README.md
@@ -1,15 +1,19 @@
 # Playing Cards Generator
 
-#### This project is about a very basic fetch of data using AJAX
+#### This project is about a very basic fetch of data using fetch API
 
 <br />
 
 #### What you can do is:
-* generate 1 or more deck of cards
-* draw 1 or more cards
-* reshuffle the deck (removes all cards displayed)
+* Generate 1 or more deck of cards (maximum allowed are 20 decks)
+  * Negative numbers in input will be converted to 1
+  * 0 as input will be converted to 1 deck or card
+
+* Draw 1 or more cards (out of bounds numbers will simply draw all cards in the decks)
+* Reshuffle the deck (removes all cards displayed)
+* Track down remaining cards number and decks 
 
 <br />
 
 #### Side notes:
-* The API from where the AJAX fetches the cards is https://deckofcardsapi.com
+* The API from where the cards are fetched  is https://deckofcardsapi.com
--- playing-cards-generator/script.js
+++ playing-cards-generator/script.js
@@ -1,45 +1,58 @@
 'use strict';
 
 const inputsContainer = document.getElementById('inputs-container'),
-    cardsTable = document.getElementById('cards-table');
-let deckNumber = ''
+    cardsTable = document.getElementById('cards-table'),
+    cardsLeftMessage = document.getElementById('remaining-cards'),
+    drawBtn = document.getElementById('draw-cards'),
+    drawContainerBtns = document.querySelectorAll('#button-area button');
+let deckNumber = '';
 
+
+// the function used to grab data from the api based on an url argument
+const fetchData = async (url) => {
+    const response = await fetch(url);
+    return await response.json();
+}
+
+// calculate how many decks are left after drawing each time
+const getDecksNumber = (numOfCards) => {
+    let count = 0;
+    while (numOfCards > 0) {
+        numOfCards -= 52
+        count++;
+    }
+    return count;
+}
 
 inputsContainer.addEventListener('click', e => {
 
-    const deckCount = document.getElementById('deck-no').value;
-    const drawCount = document.getElementById('draw').value;
-
+    /*
+        i used Number and to string, in case there will be cases of 0 as first digit,
+          Number converts an empty string to 0, while parseInt('') returns NaN
+     */
+    const deckCount = parseInt(document.getElementById('deck-no').value) > 0 ?
+        document.getElementById('deck-no').value : 1;
+    const drawCount = parseInt(document.getElementById('draw').value) > 0 ?
+        document.getElementById('draw').value : 1
     let url = '';
 
+    // if deck input is empty string, then default to 1 deck of card
     switch (e.target.id) {
         case 'generate-deck':
-            url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${deckCount}`;
+            url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${deckCount > 20 ? 20 : deckCount}`;
             break;
         case 'draw-cards':
             url = `https://deckofcardsapi.com/api/deck/${deckNumber}/draw/?count=${drawCount}`;
             break;
         case 'reshuffle':
-            url = `https://deckofcardsapi.com/api/deck/${deckIdSpan}/shuffle/`;
+            url = `https://deckofcardsapi.com/api/deck/${deckNumber}/shuffle/`;
             break;
         default:
             return;
     }
 
-    const xhr = new XMLHttpRequest();
-
-    xhr.open("GET", url, true);
-    xhr.send();
-
-    xhr.onerror = () => {
-        alert("Something went wrong during the transaction");
-    };
-
-    xhr.onload = function () {
-        if (this.status !== 200) {
-            alert(`Something went wrong. the code is ${this.status} with the following: ${this.responseText}`);
-        }
-        const data = JSON.parse(this.responseText);
+    // fetch data from the api and populate the page with cards based on the request
+    fetchData(url).then(data => {
         deckNumber = data.deck_id;
 
         if (url.includes('/draw/')) {
@@ -50,11 +63,48 @@ inputsContainer.addEventListener('click', e => {
                 cardImage.name = card.suit;
                 cardImage.alt = card.code;
 
-                cardsTable.appendChild(cardImage);
+                const cardInfo = document.createElement('DIV');
+                cardInfo.classList.add('card-title');
+                cardInfo.textContent = `${isNaN(card.value) ? card.value[0] : card.value} of ${card.suit}`;
+
+                const cardsDetails = document.createElement('DIV');
+                cardsDetails.classList.add('card-details');
+
+                cardsDetails.appendChild(cardInfo);
+                cardsDetails.appendChild(cardImage);
+                cardsTable.appendChild(cardsDetails);
             }
         } else if (url.includes('shuffle')) {
-            //TODO
-        }
-    };
 
+            // remove all the cards when url includes shuffle, or when generate deck button is clicked again
+            while (cardsTable.children.length > 0) {
+                cardsTable.removeChild(cardsTable.firstChild);
+            }
+        }
+        const numberOfDecks = getDecksNumber(data.remaining);
+
+        // enable draw and reshuffle buttons if there are decks on table
+        if (parseInt(deckCount) >= 0) {
+            drawContainerBtns.forEach(item => item.disabled = false);
+        }
+
+        /*
+           if there are no remaining cards to draw, disable draw button and show message.
+             the show message will appear when there are no cards to draw, as well
+         */
+        if (data.remaining >= 0) {
+            if (data.remaining === 0) {
+                cardsLeftMessage.textContent = `Remaining cards: ${data.remaining}`;
+                document.querySelector('#draw-cards').disabled = true;
+
+            } else if (data.remaining > 0) {
+                cardsLeftMessage.textContent = `Remaining cards in ${numberOfDecks} decks: ${data.remaining}`;
+            }
+            cardsLeftMessage.style.visibility = 'visible';
+        }
+
+    }).catch(err => {
+        cardsLeftMessage.style.visibility = 'visible';
+        cardsLeftMessage.textContent = `Error: ${err}`;
+    });
 });
